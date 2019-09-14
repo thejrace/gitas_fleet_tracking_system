3d0ea12973a934229a3888ae2f9f989ee51740db
JesterPC <ahmetkanbur@gmail.com>
2019-09-14 16:56:44 +0300

    ui updates

--- src/main/java/ui/component/BusBox.java
+++ src/main/java/ui/component/BusBox.java
@@ -7,5 +7,24 @@
  */
 package ui.component;
 
-public class BusBox {
+import model.Bus;
+import ui.UIComponent;
+
+public class BusBox extends UIComponent {
+
+    private Bus bus;
+
+    public BusBox(Bus bus){
+        this.bus = bus;
+    }
+
+    public BusBox(){
+
+    }
+
+    public void initUI(Bus bus){
+        loadFXML("bus_box");
+        ((BusBoxController)getController()).setData(bus);
+    }
+
 }
--- src/main/java/ui/component/BusBoxController.java
+++ src/main/java/ui/component/BusBoxController.java
@@ -7,5 +7,28 @@
  */
 package ui.component;
 
-public class BusBoxController {
+import javafx.fxml.Initializable;
+import model.Bus;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BusBoxController implements Initializable {
+
+    private Bus bus;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void setData(Bus bus){
+        this.bus = bus;
+        updateUI();
+    }
+
+    private void updateUI(){
+
+    }
+
 }
