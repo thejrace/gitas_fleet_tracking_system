efc5dbc813fa5846deec3734663b8910e8d2c389
JesterPC <ahmetkanbur@gmail.com>
2019-09-25 20:29:48 +0300

    replace test config with actual data

--- src/main/java/utils/SharedConfig.java
+++ src/main/java/utils/SharedConfig.java
@@ -4,7 +4,6 @@ import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -15,38 +14,25 @@ public class SharedConfig {
     public static JSONObject DATA = new JSONObject();
 
     public static boolean read(){
-
-        JSONObject test = new JSONObject();
-        test.put("installDir", "C:/gfts/");
-        test.put("download_url", "http://gitas_api.test/storage/fts_download/");
-        JSONArray apiUrls = new JSONArray();
-        apiUrls.put("http://gitas_api.test/api/");
-        test.put("base_api", apiUrls );
-        DATA = test;
-        return true;
-
-//        if( Common.checkFile( "app_config.json" ) ){
-//            try {
-//                //DATA = new JSONObject( Common.readJSONFile("app_config.json") );
-//                JSONObject test = new JSONObject();
-//                test.put("installDir", "C:/gfts/");
-//                DATA = test;
-//            } catch (JSONException e ){
-//                e.printStackTrace();
-//            }
-//            return true;
-//        } else {
-//            Platform.runLater(() -> {
-//                Alert alert = new Alert(Alert.AlertType.ERROR);
-//                alert.setTitle("Gitaş FTS");
-//                alert.setHeaderText("Hata oluştu. Kod: CONFIG_EKSIK");
-//                alert.setContentText("Sistem yöneticisine hatayı bildirin.");
-//                ButtonType cancelBtn = new ButtonType("İptal", ButtonBar.ButtonData.CANCEL_CLOSE);
-//                alert.getButtonTypes().setAll(cancelBtn );
-//                alert.showAndWait();
-//            });
-//        }
-//        return false;
+        if( Common.checkFile( "app_config.json" ) ){
+            try {
+                DATA = new JSONObject( Common.readJSONFile("app_config.json") );
+            } catch (JSONException e ){
+                e.printStackTrace();
+            }
+            return true;
+        } else {
+            Platform.runLater(() -> {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Gitaş FTS");
+                alert.setHeaderText("Hata oluştu. Kod: CONFIG_EKSIK");
+                alert.setContentText("Sistem yöneticisine hatayı bildirin.");
+                ButtonType cancelBtn = new ButtonType("İptal", ButtonBar.ButtonData.CANCEL_CLOSE);
+                alert.getButtonTypes().setAll(cancelBtn );
+                alert.showAndWait();
+            });
+        }
+        return false;
     }
 
 }
