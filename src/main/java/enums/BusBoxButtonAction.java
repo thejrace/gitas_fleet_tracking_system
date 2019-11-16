286f093fcbf37c0120069f68854591243f0c9e9f
JesterPC <ahmetkanbur@gmail.com>
2019-11-16 19:12:01 +0300

    implement BusBoxButton ui component

---
+++ src/main/java/enums/BusBoxButtonAction.java
@@ -0,0 +1,18 @@
+/*
+ *  Gitas Fleet Tracking System 2019
+ *
+ *  Contributors:
+ *      - Ahmet Ziya Kanbur
+ *
+ */
+package enums;
+
+public enum BusBoxButtonAction {
+    PLAN,
+    MESSAGES,
+    DRIVER_INFO,
+    STATS,
+    IYS,
+    MAP,
+    NOTES
+}
---
+++ src/main/java/ui/custom_control/BusBoxButton.java
@@ -0,0 +1,58 @@
+/*
+ *  Gitas Fleet Tracking System 2019
+ *
+ *  Contributors:
+ *      - Ahmet Ziya Kanbur
+ *
+ */
+package ui.custom_control;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import enums.BusBoxButtonAction;
+import javafx.beans.NamedArg;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+
+import java.io.IOException;
+
+public class BusBoxButton extends Button {
+
+    @FXML
+    private FontAwesomeIconView icon;
+
+    /**
+     * Constructor
+     *
+     * @param tooltipText
+     * @param iconName
+     * @param iconName
+     */
+    public BusBoxButton(@NamedArg("tooltipText") String tooltipText, @NamedArg("iconName") String iconName, @NamedArg("action") int action ){
+        // load layout
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+                "/fxml/custom_controls/BusBoxButton.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+
+            setText("");
+
+            icon.setGlyphName(iconName);
+            icon.setFill(Color.valueOf("#fff"));
+
+            // click action
+            setOnAction( ev -> {
+
+                System.out.println(BusBoxButtonAction.values()[action]);
+
+            });
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
